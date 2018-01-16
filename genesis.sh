ca3735e79a485756d70b85b9b70a69e951c01358
sunny.luo <sunnylqm@qq.com>
2018-01-16 16:19:45 +0800

    Make networkid more unique

--- genesis.sh
+++ genesis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 GEN_NONCE="0xeddeadbabeedffff"
-GEN_CHAIN_ID=1827
+GEN_CHAIN_ID=16191827
 GEN_ALLOC='"0x0e6cb909783e9c6e3a1f34547b253272a33b2b10": {"balance": "100000"}'
 sed "s/\${GEN_NONCE}/$GEN_NONCE/g" src/genesis.json.template | sed "s/\${GEN_ALLOC}/$GEN_ALLOC/g" | sed "s/\${GEN_CHAIN_ID}/$GEN_CHAIN_ID/g" > genesis.json
